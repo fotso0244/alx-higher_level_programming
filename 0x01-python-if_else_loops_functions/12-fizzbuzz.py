d18de35233987b4e259ef29ed34529785b46e566
fotso aristide <fotso0244@gmail.com>
2022-06-29 14:19:44 +0000

    How to print numbers with mix fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz", end=' ')
         if (i % 5 == 0):
             print("Buzz", end=' ')
-        if (i % 5 == 0 and i % 3 == 0)
+        if (i % 5 == 0 and i % 3 == 0):
             print("FizzBuzz", end='')
         elif (i % 3 != 0 and i % 5 != 0):
             print(i, end=' ')
