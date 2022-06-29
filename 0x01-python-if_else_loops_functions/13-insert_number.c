cca077885ae27b0bf3db7005b0c0fd557e4f7819
fotso aristide <fotso0244@gmail.com>
2022-06-29 16:23:19 +0000

    How to insert a number into a linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,12 +14,12 @@ listint_t *insert_node(listint_t **head, int number)
         listint_t *curr, *new;
 
         curr = *head;
+	new = malloc(sizeof(*new));
+	new->n = number;
         while (curr != NULL)
         {
                 if (curr->n <= number && curr->next->n >= number)
                 {
-                        new = malloc(sizeof(*new));
-                        new->n = number;
                         new->next = curr->next;
                         curr->next = new;
                         new = NULL;
@@ -27,7 +27,22 @@ listint_t *insert_node(listint_t **head, int number)
                         break;
                 }
                 else
-                        curr = curr->next;
+			if (curr->n >= number)
+			{
+				new->next = curr;
+				*head = new;
+				curr = NULL;
+				new = NULL;
+				break;
+			}
+			else
+                        	curr = curr->next;
+		if (curr->next == NULL)
+		{
+			new->next = NULL;
+			curr->next = new;
+			curr = NULL;
+		}
         }
         return (*head);
 }
