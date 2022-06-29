881df456aed5584549a4839bd7500342ff26f0bb
fotso aristide <fotso0244@gmail.com>
2022-06-29 01:40:11 +0000

    How to print last digit of an integer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-mod = number % 10
-print("Last digit of", number, "is")
+if number < 0:
+    mod = ((number * -1) % 10) * -1
+else:
+    mod = number % 10
+print("Last digit of", number, "is", end=' ')
 if (mod < 6 and mod != 0):
     print(mod, "and is less than 6 and not 0", end='\n')
 if mod > 5:
