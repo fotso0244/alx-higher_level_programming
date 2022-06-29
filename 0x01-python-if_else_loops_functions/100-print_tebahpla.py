ef6e732e68d639daa15956be82cd9098efa358a2
fotso aristide <fotso0244@gmail.com>
2022-06-29 18:54:18 +0000

    How to print alphabet in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
-    if (i % 2 == 0):
-        print(chr(i), end='')
-    else:
-        print(chr(i - 32), end='')
+    print(chr(i) if i % 2 == 0 else chr(i - 32), end='')
