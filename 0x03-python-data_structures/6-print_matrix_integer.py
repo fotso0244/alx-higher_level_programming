96726187cd66084387c6055972d7d15b3e710cd6
aristide <fotso0244@gmail.com>
2022-07-06 03:42:27 +0000

    Update 6-print...

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if (len(matrix) != 0 and matrix is not None):
         for row in matrix:
-            for i in range(len(row)):
-                if (i != len(row) - 1):
-                    print("{:d}".format(row[i]), end=' ')
-                else:
-                    print("{:d}".format(row[i]))
+            if row != []:
+                for i in range(len(row)):
+                    if (i != len(row) - 1):
+                        print("{:d}".format(row[i]), end=' ')
+                    else:
+                        print("{:d}".format(row[i]))
+            else:
+                print("\n", end='')
