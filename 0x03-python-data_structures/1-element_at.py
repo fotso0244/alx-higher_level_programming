71c261097d59f01c7dbbe8b4b17e73f7951d905f
aristide <fotso0244@gmail.com>
2022-07-06 01:58:53 +0000

    How to retrieve an element of a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    try:
-        if (idx < 0 or idx > len(my_list)):
-            print("None")
-        else:
-            return my_list[idx]
-    except:
-        print("An error occured")
+    if (idx < 0 or idx > len(my_list)):
+        print("None")
+    else:
+        return my_list[idx]
