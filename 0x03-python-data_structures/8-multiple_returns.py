242d10aba0d3a2c183d9e75b1316a2784122b1f9
aristide <fotso0244@gmail.com>
2022-07-06 05:03:22 +0000

    How to make multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (sentence != None and len(sentence) != 0):
+    if (sentence is not None and len(sentence) != 0):
         length = len(sentence)
         first = sentence[0]
     else:
