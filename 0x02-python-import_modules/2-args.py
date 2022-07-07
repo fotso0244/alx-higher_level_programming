72ad961ac5c77a4504a0705b84e8461260bd219d
aristide <fotso0244@gmail.com>
2022-07-07 05:20:16 +0000

    How to retrieve arguments of a script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 import sys
 if __name__ == "__main__":
     l = len(sys.argv)
-    print("{:d} argument {:s}".format(l - 1, "." if l == 1 else ":"))
+    print("{:d} argument{:s}".format(l - 1, "." if l == 1 else ":"))
     if (l != 1):
         for i in range(l):
             if i != 0:
-                print("{:d}:{}".format(i, sys.argv[i]))
+                print("{:d}: {}".format(i, sys.argv[i]))
