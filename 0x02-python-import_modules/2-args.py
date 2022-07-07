ed2ceb883e5e2ab10f80a81023d8880bc0370de0
aristide <fotso0244@gmail.com>
2022-07-07 05:14:40 +0000

    How to retrieve arguments of a script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    l = len(sys.argv)
+    print("{:d} arguments {:s}".format(l - 1, "." if l == 1 else ":"))
+    if (l != 1):
+        for i in range(l):
+            if i != 0:
+                print("{:d}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,2 +1,3 @@
 Script 0-add.py adds 2 integers
 Script 1-calculation.py processes some basic operations on 2 integers
+Script 2-add retrieves all arguments of a script
