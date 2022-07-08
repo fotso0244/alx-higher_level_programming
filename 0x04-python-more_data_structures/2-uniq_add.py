8622648f3a846612731fc319b2e8ccdaa99981fe
aristide <fotso0244@gmail.com>
2022-07-08 22:54:14 +0000

    How to sum uniq elements of a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def check_item(my_list, item):
+    if len(my_list) != 0:
+        for i in my_list:
+            if i == item:
+                return 1
+    return 0
+
+
+def uniq_items(my_list):
+    if len(my_list) != 0:
+        uniq = []
+        for i in my_list:
+            idx = my_list.index(i)
+            if (check_item(uniq, my_list[idx]) == 0):
+                uniq.append(my_list[idx])
+    return uniq
+
+def uniq_add(my_list=[]):
+    uniq = uniq_items(my_list)
+    res = 0
+    for i in uniq:
+        res += i
+    return res
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,2 +1,3 @@
 Script 0-square_matrix_simple returns a square of a matrix
 Script 1-search_replace replace all occurrences of an element
+Script 2-uniq_add adds uniq elements of a list
