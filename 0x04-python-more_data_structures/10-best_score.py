1d52c6c864a0900036967e12e31d6a42851e9239
aristide <fotso0244@gmail.com>
2022-07-09 02:27:55 +0000

    How to get a key with best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     i = 0
     key = None
-    if a_dictionary != None:
+    if a_dictionary is not None:
         item = a_dictionary.items()
         for x in item:
             if x[1] > i:
